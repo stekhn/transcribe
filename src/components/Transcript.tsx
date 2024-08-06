39b5135bad976aa2b0517f9479fd08969b818e5a
Steffen Kühne <stekhn@gmail.com>
2024-08-06 18:35:13 +0200

    Add SRT export

--- src/components/Transcript.tsx
+++ src/components/Transcript.tsx
@@ -19,9 +19,34 @@ export default function Transcript({ transcribedData }: Props) {
         link.click();
         URL.revokeObjectURL(url);
     };
+    const formatTime = (seconds: number): string => {
+        const date = new Date(0);
+        date.setSeconds(seconds);
+        const milliseconds = seconds * 1000;
+        return (
+            date.toISOString().substr(11, 8) +
+            "," +
+            ("000" + (milliseconds % 1000)).slice(-3)
+        );
+    };
+    const exportSRT = () => {
+        const jsonData = transcribedData?.chunks ?? [];
+        const srt = jsonData
+            .map((entry, index) => {
+                const start = formatTime(entry.timestamp[0] as number);
+                const end = formatTime(entry.timestamp[1] as number);
+                const text = entry.text.trim();
+                return `${index + 1}\n${start} --> ${end}\n${text}\n`;
+            })
+            .join("\n");
+
+        const blob = new Blob([srt], { type: "text/plain" });
+        saveBlob(blob, "transcript.srt");
+    };
+
     const exportTXT = () => {
-        let chunks = transcribedData?.chunks ?? [];
-        let text = chunks
+        const chunks = transcribedData?.chunks ?? [];
+        const text = chunks
             .map((chunk) => chunk.text)
             .join("")
             .trim();
@@ -29,6 +54,7 @@ export default function Transcript({ transcribedData }: Props) {
         const blob = new Blob([text], { type: "text/plain" });
         saveBlob(blob, "transcript.txt");
     };
+
     const exportJSON = () => {
         let jsonData = JSON.stringify(transcribedData?.chunks ?? [], null, 2);
 
@@ -92,6 +118,12 @@ export default function Transcript({ transcribedData }: Props) {
                             className='text-white bg-teal-500 hover:bg-teal-600 focus:ring-4 focus:ring-teal-300 font-medium rounded-lg text-sm px-4 py-2 text-center ml-2 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800 inline-flex items-center'
                         >
                             Export TXT
+                        </button>
+                        <button
+                            onClick={exportSRT}
+                            className='text-white bg-teal-500 hover:bg-teal-600 focus:ring-4 focus:ring-teal-300 font-medium rounded-lg text-sm px-4 py-2 text-center ml-2 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800 inline-flex items-center'
+                        >
+                            Export SRT
                         </button>
                         <button
                             onClick={exportJSON}
